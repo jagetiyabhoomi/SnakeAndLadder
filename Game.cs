0fc78f8c00a28dcac70f77989eead553fe342303
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-27 13:48:22 +0530

    Rolls the die

---
+++ Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Game
+    {
+        public int position = 0;
+        public void StartTheGame()
+        {
+            Random random = new Random();
+            int num = random.Next(1, 6);
+            position = num;
+
+        }
+    }
+}
