728a6af1c43b85f5aa713b6ff3fe7114d33add20
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-27 13:47:56 +0530

    Usecase 2

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Start game at 0 Position");
+            Game game = new Game();
+            game.StartTheGame();
         }
     }
 }
