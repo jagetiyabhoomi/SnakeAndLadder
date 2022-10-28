3535cdcff62da4cd4b39765b952b0ac040b6dbed
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-28 11:33:37 +0530

    Usecase 6

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Start game at 0 Position");
-            Console.WriteLine("Usecase 5");
+            Console.WriteLine("Usecase 6");
             Game game = new Game();
             game.StartTheGame();
         }
