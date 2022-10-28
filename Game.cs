12948f93cce543d5f2bba773de52216c613ad257
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-28 10:43:19 +0530

    Play untill reach 100

--- Game.cs
+++ Game.cs
@@ -14,24 +14,27 @@ namespace SnakeAndLadder
             Random random = new Random();
             int num = random.Next(1, 6);
             position = num;
-            //while (position <= 100)
-           // {
+            while (position <= 100)
+            {
                 int next = random.Next(1, 3);
                 switch(next)
                 {
                     case 0:
                         Console.WriteLine("This is No Play");
                         position = position;
+                        Console.WriteLine("Current Position: " + position);
                         break;
                     case 1:
                         Console.WriteLine("This is Lader");
                         int val =random.Next(1, 6);
                         position = position + val;
+                        Console.WriteLine("Current Position: " + position);
                         break;
                     case 2:
                         Console.WriteLine("This is Snake");
                         int val2 = random.Next(1, 6);
                         position=position - val2;
+                        Console.WriteLine("Current Position: " + position);
                         break;
                     default:
                         Console.WriteLine("Nothing");
@@ -40,8 +43,12 @@ namespace SnakeAndLadder
                 }
 
 
-           // }
-            Console.WriteLine("Current Position: " + position);
+             }
+            if (position < 0)
+            { position = 0; }
+            if(position > 100)
+            { position = 100; }
+            Console.WriteLine("Final Position: " + position);
         }
     }
 }
