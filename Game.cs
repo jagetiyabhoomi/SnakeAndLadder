47e5e8949a697c0354fed9c202e39e0871cc7cf7
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-28 10:25:24 +0530

    added Usecase 3

--- Game.cs
+++ Game.cs
@@ -14,7 +14,34 @@ namespace SnakeAndLadder
             Random random = new Random();
             int num = random.Next(1, 6);
             position = num;
+            //while (position <= 100)
+           // {
+                int next = random.Next(1, 3);
+                switch(next)
+                {
+                    case 0:
+                        Console.WriteLine("This is No Play");
+                        position = position;
+                        break;
+                    case 1:
+                        Console.WriteLine("This is Lader");
+                        int val =random.Next(1, 6);
+                        position = position + val;
+                        break;
+                    case 2:
+                        Console.WriteLine("This is Snake");
+                        int val2 = random.Next(1, 6);
+                        position=position - val2;
+                        break;
+                    default:
+                        Console.WriteLine("Nothing");
+                        break;
 
+                }
+
+
+           // }
+            Console.WriteLine("Current Position: " + position);
         }
     }
 }
